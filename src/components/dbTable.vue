c4ea9f7638eb7533e7ce24fb935afad589355dda
lliyu <1171758781@qq.com>
2018-11-10 23:10:57 +0800

    路由

--- src/components/dbTable.vue
+++ src/components/dbTable.vue
@@ -19,6 +19,7 @@
   table.render({
     elem: '#test'
     ,url:'/dbs/list'
+    ,page:true
     ,cellMinWidth: 80 //全局定义常规单元格的最小宽度，layui 2.2.1 新增
     ,cols: [[
       {field:'id', width:80, title: 'ID', sort: true}

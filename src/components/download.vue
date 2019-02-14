81f6ab4fcd44ac597c0f202541bd62eb52424537
lliyu <1171758781@qq.com>
2019-02-14 16:59:29 +0800

    文件下载页面

---
+++ src/components/download.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+<form action="/dbs/download" method="post" enctype="multipart/form-data">
+        <p>选择文件: <input type="file" name="file"/></p>
+        <p><input type="submit" value="提交"/></p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "download",
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+
+}
+</script>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import dbTable from '@/components/dbTable'
+import download from '@/components/download'
 
 Vue.use(Router)
 
@@ -8,12 +9,17 @@ export default new Router({
   routes: [
     {
       path: '/',
-      redirect: '/dbas'
+      redirect: '/download'
     },
     {
       path: '/dbas',
       name: 'dbTable',
       component: dbTable
+    },
+    {
+      path: '/download',
+      name: 'download',
+      component: download
     }
   ]
 })
